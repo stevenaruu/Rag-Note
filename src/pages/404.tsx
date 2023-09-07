3beb379b261dc32fc6f71fe93ae138d294638682
Steven Forsythia <stvnhd164@gmail.com>
2023-09-07 15:17:44 +0700

    fix: unresponsive content

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -9,7 +9,7 @@ const _404Page = () => {
                 <></>
             </Sidebar>
             <div className="h-full w-4/6 fixed top-0 overflow-x-hidden right-0 flex justify-center flex-col items-center bg-gray-200">
-                <div className="text-emerald-600">
+                <div className="text-emerald-600 p-10">
                     <p className="text-9xl font-bold">404</p>
                     <p className="text-3xl mt-5 mb-2 font-bold">Oops! You&apos;re lost.</p>
                     <p className="text-lg font-semibold">The page you are looking for does not exist. How you got here is a mystery. But you can click the button below to go back to the application.
